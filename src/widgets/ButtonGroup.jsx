2993c181bc702c98b688a70358391a94f5f68945
Kirill Sukhomlin <ya@kirilloid.ru>
2017-10-11 02:09:05 +0300

    make button-group select active by value, rather than by index

--- src/widgets/ButtonGroup.jsx
+++ src/widgets/ButtonGroup.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 export default class extends Component {
     render() {
         return <div className="button-group">{
-            this.props.buttons.map(({ content, value, title }, index) => 
+            this.props.buttons.map(({ content, value, title }) => 
                 <button value={value}
                         key={value}
                         title={title || ""}
-                        className={index === this.props.value ? 'active' : ''}
+                        className={value === this.props.value ? 'active' : ''}
                         onClick={event => this.props.onChange(event.currentTarget.value)}>
                     {content}
                 </button>
