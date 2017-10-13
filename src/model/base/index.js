c1377925a3f44a70706c6292d78a011b72430a97
Kirill Sukhomlin <ya@kirilloid.ru>
2017-10-13 15:27:08 +0300

    research requirements included into units

--- src/model/base/index.js
+++ src/model/base/index.js
@@ -1,7 +1,43 @@
+import { extend } from '../../utils';
+
 import units from './units';
-import buildings from './buildings';
+import buildings, { ID } from './buildings';
 
 export default {
-    units,
+    units: extend(units, [
+        [   { p: [ID.BARRACKS], },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 1, [ID.BLACKSMITH]: 1} },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 5, [ID.BLACKSMITH]: 1} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 1, [ID.STABLES]: 1} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 5} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 10} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:10, [ID.WORKSHOP]: 1} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:15, [ID.WORKSHOP]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE], r: {[ID.ACADEMY]:20, [ID.RALLY_POINT]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE] },
+        ],
+        [   { p: [ID.BARRACKS], },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 1, [ID.BARRACKS]: 3} },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 3, [ID.BLACKSMITH]: 1} },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 1, [ID.MAIN_BUILDING]: 5} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 3} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]:15, [ID.STABLES]: 10} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:10, [ID.WORKSHOP]: 1} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:15, [ID.WORKSHOP]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE], r: {[ID.ACADEMY]:20, [ID.RALLY_POINT]: 5} },
+            { p: [ID.RESIDENCE, ID.PALACE] },
+        ],
+        [   { p: [ID.BARRACKS], },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 3, [ID.BLACKSMITH]: 3} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 1} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 3} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 5} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]:15, [ID.STABLES]:10} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:10, [ID.WORKSHOP]: 1} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:15, [ID.WORKSHOP]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE], r: {[ID.ACADEMY]:20, [ID.RALLY_POINT]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE] },
+        ],
+    ]),
     buildings
 };
--- src/model/t4.fs/index.js
+++ src/model/t4.fs/index.js
@@ -1,7 +1,32 @@
+import { extend } from '../../utils';
+
 import units from './units';
 import buildings from './buildings';
 
 export default {
-    units,
+    units: extend(units, {
+        5:[ { p: [ID.BARRACKS], },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 1, [ID.BLACKSMITH]: 1} },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 5, [ID.BLACKSMITH]: 1} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 1} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 5} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 10} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:10, [ID.WORKSHOP]: 1} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:15, [ID.WORKSHOP]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE], r: {[ID.ACADEMY]:20, [ID.RALLY_POINT]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE] },
+        ],
+        6:[ { p: [ID.BARRACKS], },
+            { p: [ID.BARRACKS], r: {[ID.ACADEMY]: 3, [ID.BLACKSMITH]: 1} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 1} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 3} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]: 5, [ID.STABLES]: 5} },
+            { p: [ID.STABLES],  r: {[ID.ACADEMY]:15, [ID.STABLES]:10} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:10, [ID.WORKSHOP]: 1} },
+            { p: [ID.WORKSHOP], r: {[ID.ACADEMY]:15, [ID.WORKSHOP]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE], r: {[ID.ACADEMY]:20, [ID.RALLY_POINT]:10} },
+            { p: [ID.RESIDENCE, ID.PALACE] },
+        ],
+    },
     buildings
 };
