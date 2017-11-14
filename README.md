3e5f4a30896079a94aa48d6504f6113e736ec04b
Kirill Sukhomlin <ya@kirilloid.ru>
2017-11-14 23:01:25 +0300

    Full-featured conquering calculator (except state restore)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Travian _0.8.8_
+# Travian _0.8.9_
 Set of utilities for the Travian online game.<br>
 Will be available around Q1 2018 on the main site: travian.kirilloid.ru<br>
 Meanwhile work-in-progress could be seen [here](http://kirilloid.ru/travian2/)
--- src/components/Conq.jsx
+++ src/components/Conq.jsx
@@ -1,51 +1,133 @@
 import React, { Component } from 'react';
+import update from 'react-addons-update';
 
-function timesToConq({ units }, { loyalty, admins, tribe }) {
-    const [baseMin, baseMax] = units[tribe][8].l;
-    const min = Math.ceil(loyalty / (admins * baseMax));
-    const max = Math.ceil(loyalty / (admins * baseMin));
-    return (min === max) ? max : `${min}–${max}`;
+import { roundP, limit, compose } from '../utils';
+import { numericInt } from '../utils/probability';
+
+const popMalus = compose(roundP(1e-3), limit(0.667, 1));
+
+function timesToConq({ units }, { loyalty, def, off }) {
+    const pairs = [];
+    let totalMin = 0;
+    let totalMax = 0;
+    const steps = off.map(attacker => {
+        const malus = popMalus((def.pop / attacker.pop) ** 0.2)
+                    * (attacker.brew ? 0.5 : 1);
+        const party = (attacker.party - def.party) * 5;
+        let [min, max] = units[attacker.tribe][8].l
+            .map(value => (value + party) * malus * attacker.admins);
+        pairs.push({ min, max });
+        totalMin += min;
+        totalMax += max;
+        return numericInt(pairs);
+    });
+    return {
+        steps,
+        total(loyalty) {
+            const min = Math.ceil(loyalty / totalMax);
+            const max = Math.ceil(loyalty / totalMin);
+            return (min === max) ? max : `${min}–${max}`;
+        }
+    }
+}
+
+function formatPercent(f, loyalty) {
+    const number = f(loyalty);
+    if (f(loyalty + 1) === 1) return "100%";
+    if (f(loyalty - 1) === 1) return "\u{2248}100%";
+    return (number * 100).toFixed(2).replace(/\.?0+$/, '') + '%';
 }
 
 export default class Conq extends Component {
     constructor() {
         super();
+        this.aid = 0;
+        this.lastOff = {
+            id: this.aid++,
+            pop: 3000,
+            tribe: 0,
+            admins: 1,
+            party: false,
+            brew: false,
+        };
         this.state = {
             loyalty: 100,
-            admins: 1,
-            tribe: 0
+            def: {
+                pop: 1000,
+                party: false,
+            },
+            off: [this.lastOff]
         };
-        this.setAdmins  = this.set.bind(this, 'admins');
-        this.setTribe   = this.set.bind(this, 'tribe');
-        this.setLoyalty = this.set.bind(this, 'loyalty');
     }
-    set(fieldName, value) {
-        this.setState({ ...this.state, [fieldName]: value });
+    set(fieldNames, value) {
+        if (fieldNames.length === 3
+        &&  fieldNames[0] === 'off'
+        &&  fieldNames[1] === this.state.off.length - 1) {
+            this.lastOff[fieldNames[2]] = value;
+        }
+        this.setState(update(
+            this.state, 
+            fieldNames.reduceRight(
+                (obj, name) => ({ [name]: obj }),
+                { $set: value }
+            )
+        ));
+    }
+    addOff() {
+        this.lastOff = update(this.lastOff, { id: { $set: this.aid++ } })
+        this.setState(update(
+            this.state, 
+            { off: { $push: [this.lastOff] } }
+        ));        
+    }
+    removeOff(i) {
+        this.setState(update(
+            this.state, 
+            { off: { $splice: [[i, 1]] } }
+        ));        
     }
     render() {
         const { model, lang } = this.props;
+        const conqStats = timesToConq(model, this.state);
         return (<div>
+            <label htmlFor="def_pop">{lang('terms.population')}</label>
+            <input type="number" id="def_pop"
+                min="1" max="99999" step="100" value={this.state.def.pop}
+                onChange={event => this.set(['def', 'pop'], +event.target.value)} />
             <label htmlFor="loyalty">{lang('terms.loyalty')}</label>
             <input type="range" id="loyalty"
                 min="0" max="200" step="1" value={this.state.loyalty}
-                onChange={event => this.setLoyalty(+event.target.value)} />
+                onChange={event => this.set(['loyalty'], +event.target.value)} />
             <output>{this.state.loyalty}</output>
             <br />
-            <label>
+            {lang('conq_chance')}
+            {this.state.off.map((attacker, i) => <div key={attacker.id}>
+                <button onClick={() => this.removeOff(i)}>–</button>
+                <label htmlFor={`off_pop_${i}`}>
+                    {lang('terms.population')}
+                </label>
+                <input type="number"
+                    id={`off_pop_${i}`}
+                    min="1" max="99999" step="100" value={attacker.pop}
+                    onChange={event => this.set(['off', i, 'pop'], +event.target.value)} />
                 <input type="number" min="1" max="3"
-                    value={this.state.admins}
-                    onChange={event => this.setAdmins(+event.target.value)} />
+                    value={attacker.admins}
+                    onChange={event => this.set(['off', i, 'admins'], +event.target.value)} />
                 &times;
-                <select value={this.state.tribe}
-                    onChange={event => this.setTribe(+event.target.value)}>
+                <select value={attacker.tribe}
+                    onChange={event => this.set(['off', i, 'tribe'], +event.target.value)}>
                     {model.units.map((raceUnits, index) => raceUnits[8].l
                         ? <option value={index} key={index}>
                             {lang(`objects.troops.t_${index}_8`)}
                             </option>
                         : null)}
                 </select>
-            </label>
-            {lang('conq_times', [timesToConq(model, this.state)])}
+                |
+                <span>{formatPercent(conqStats.steps[i], this.state.loyalty)}</span>
+            </div>)}
+            <button onClick={() => this.addOff()}>+</button>
+            <span dangerouslySetInnerHTML={{
+                __html: lang('conq_times', [conqStats.total(this.state.loyalty)])}} />
         </div>);
     }
 }
